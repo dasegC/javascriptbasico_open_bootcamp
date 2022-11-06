0d3cca5f6ef56f3ae62c02eb1409ae68834c7ca7
Dario Gimenez <daseg2109@gmail.com>
2022-11-06 09:42:12 -0300

    first commit

---
+++ mi_primer_proyecto-de-node/index.js
@@ -0,0 +1,9 @@
+// Este es el menasaje de bienvenida
+console.log('Esta es la puerta de entrada al proyecto');
+/*
+    Este es un comentario
+    multilinea
+*/
+
+// Este es el mensaje que se envia para despedir
+console.log('Adios');
