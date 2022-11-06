573afcf06d15847df2a57917d1aff3940a49bb73
Dario Gimenez <daseg2109@gmail.com>
2022-11-06 12:43:02 -0300

    Ejercicio Sesion 2

---
+++ ejercicios/02-sintaxis/sintaxis.js
@@ -0,0 +1,14 @@
+const lista = [
+    "Daro Gmnz",
+    32,
+    true,
+    new Date(1986, 09, 20),
+    libro = {
+        titulo: "The maze runner",
+        autor: "James Dashner",
+        fecha: new Date(2009, 0, 1),
+        url: "https://es.wikipedia.org/wiki/The_Maze_Runner_(trilog%C3%ADa)"
+    }
+];
+
+//console.log(lista[4].titulo);
