2c6f55f511523cf57ced6b57cbfe1cf199a5339c
Dario Gimenez <daseg2109@gmail.com>
2022-11-08 07:29:13 -0300

    sesion 05

---
+++ ejercicios/05_numbers/numbers.js
@@ -0,0 +1,11 @@
+let altura_cm = 170;
+let altura_m = 1.70;
+let peso_kg = 54.5;
+
+let altura_redondeado = Math.ceil(altura_m);
+//console.log(altura_redondeado)
+let peso_redondeado = Math.floor(peso_kg);
+//console.log(peso_redondeado);
+
+let iguales = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
+//console.log(iguales);
---
+++ sesion_05_numbers/declaracion_variables_numericas.js
@@ -0,0 +1,13 @@
+// Sesion 05 - Numbers
+// Declaración de variables númericas
+let a = 5;
+console.log(a);
+
+let b = 0.1;
+console.log(b);
+
+// Precisión
+let c = 0.2;
+console.log(b + c);
+
+console.log(Math.round((0.1 + 0.2)*100)/100);
